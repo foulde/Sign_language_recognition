bc67957b937881eac959b6603d8544c5b74ef152
foulde <72756151+foulde@users.noreply.github.com>
2022-01-31 11:31:22 +0100

    Update model_argentin.py

--- model_argentin.py
+++ model_argentin.py
@@ -35,24 +35,6 @@ class SLRGRU(nn.Module):
         return hidden
     
     
-# def train(train_loader , learn_rate ,hidden_dim = 64 ,epoch = 10 )
-    
-#     model = GRU(input_dim, hidden_dim, output_dim, n_layers)
-        
-#     output_dim = 1
-#     n_layers = 2
-#     criterion = nn.MSELoss()
-#     optimizer = torch.optim.Adam(MODEL)
-#     train_loader.glos
-
-
-
-
-
-
-
-
-#1629
 
 
 def train(train_loader, learn_rate,hidden_dim=64, EPOCHS=50, model_type="GRU",input_dim = 258,output_dim =65):
@@ -124,138 +106,10 @@ def train(train_loader, learn_rate,hidden_dim=64, EPOCHS=50, model_type="GRU",in
         # print("Accuracy = {}".format(accuracy))
     torch.save(model.state_dict(), 'model_e{EPOCHS}.pth')
     return model
-    #
     
     
     
-
-# def test(model, test_loader):
-#     # set model as testing mode
-#     model.eval()
-
-#     val_loss = []
-#     all_y = []
-#     all_y_pred = []
-#     all_video_ids = []
-#     all_pool_out = []
-
-#     num_copies = 4
-
-#     with torch.no_grad():
-#         for batch_idx, data in enumerate(test_loader):
-#             print('starting batch: {}'.format(batch_idx))
-#             # distribute data to device
-#             X, y, video_ids = data
-#             # X, y = X.cuda(), y.cuda().view(-1, )
-
-#             all_output = []
-
-#             # stride = X.size()[2] // num_copies
-#             output = model(X)
-#             y_pred = 
-#             for i in range(num_copies):
-#                 X_slice = X[:, :, i * stride: (i + 1) * stride]
-#                 output = model(X_slice)
-#                 all_output.append(output)
-
-#             all_output = torch.stack(all_output, dim=1)
-#             output = torch.mean(all_output, dim=1)
-
-#             y_pred = output.max(1, keepdim=True)[1]  # (y_pred != output) get the index of the max log-probability
-
-#             # collect all y and y_pred in all batches
-#             all_y.extend(y)
-#             all_y_pred.extend(y_pred)
-#             all_video_ids.extend(video_ids)
-#             all_pool_out.extend(output)
-
-#     # compute accuracy
-#     all_y = torch.stack(all_y, dim=0)
-#     all_y_pred = torch.stack(all_y_pred, dim=0).squeeze()
-#     # all_pool_out = torch.stack(all_pool_out, dim=0).cpu().data.numpy()
-
-#     # # log down incorrectly labelled instances
-#     # incorrect_indices = torch.nonzero(all_y - all_y_pred).squeeze().data
-#     # incorrect_video_ids = [(vid, int(all_y_pred[i].data)) for i, vid in enumerate(all_video_ids) if
-#     #                        i in incorrect_indices]
-
-#     all_y = all_y.cpu().data.numpy()
-#     all_y_pred = all_y_pred.cpu().data.numpy()
-
-#     # top-k accuracy
-#     top1acc = accuracy_score(all_y, all_y_pred)
-#     # top3acc = compute_top_n_accuracy(all_y, all_pool_out, 3)
-#     # top5acc = compute_top_n_accuracy(all_y, all_pool_out, 5)
-#     # top10acc = compute_top_n_accuracy(all_y, all_pool_out, 10)
-#     # top30acc = compute_top_n_accuracy(all_y, all_pool_out, 30)
-
-#     # show information
-#     print('\nVal. set ({:d} samples): top-1 Accuracy: {:.2f}%\n'.format(len(all_y), 100 * top1acc))
-#     # print('\nVal. set ({:d} samples): top-3 Accuracy: {:.2f}%\n'.format(len(all_y), 100 * top3acc))
-#     # print('\nVal. set ({:d} samples): top-5 Accuracy: {:.2f}%\n'.format(len(all_y), 100 * top5acc))
-#     # print('\nVal. set ({:d} samples): top-10 Accuracy: {:.2f}%\n'.format(len(all_y), 100 * top10acc))
-
-
-# def compute_top_n_accuracy(truths, preds, n):
-#     best_n = np.argsort(preds, axis=1)[:, -n:]
-#  print("Total Training Time: {} seconds".format(str(sum(epoch_times))))
-#     return model
-
-
-
-# def test (model, test_loader):
-#     model.eval()
-
-#     val_loss = []
-#     all_y = []
-#     all_y_pred = []
-#     for idx,(x, label ,id) in enumerate(train_loader):
     
-#             # print(idx)
-#             # print(x.shape)
-#             counter += 1
-#             if model_type == "GRU":
-#                 h = h.data
-#             # else:
-#             #     h = tuple([e.data for e in h])
-#             model.zero_grad()
-#             # print(label)
-            
-#             out, h = model(x.float(), h)
-#             loss = criterion(out, label)
-            
-#             loss.backward()
-#             optimizer.step()
-#             avg_loss += loss.it(1, kem()
-#             y_pred = out.maxeepdim=True)[1]
-#             all_y.extend(label)
-#             all_y_pred.extend(y_pred)
-#             # correct += (out == label).float().sum()
-#         all_y = torch.stack(all_y, dim=0)
-#         all_y_pred = torch.stack(all_y_pred, dim=0).squeeze()
-        
-#         top1acc = accuracy_score(all_y, all_y_pred)*100
-#         print("ejhfejh",top1acc)
-#         #     if counter%200 == 0:
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
